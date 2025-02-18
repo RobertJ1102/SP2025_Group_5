b805bc98530975044de626b2cfb21b8dee477bce
RobertJ1102 <robert.jacobs@wustl.edu>
2025-02-18 14:21:56 -0600

    building replica API

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,6 @@
 """This module contains the SQLAlchemy model for the User table."""
+from typing import List
+from pydantic import BaseModel
 from sqlalchemy import Column, Integer, String
 from .database import Base
 
@@ -10,3 +12,19 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String(100), unique=True, index=True)
     password = Column(String(255))
+
+class PriceEstimate(BaseModel):
+    """ PriceEstimate model """
+    localized_display_name: str
+    distance: float
+    display_name: str
+    product_id: str
+    high_estimate: float
+    low_estimate: float
+    duration: int
+    estimate: str
+    currency_code: str
+
+class PriceEstimatesResponse(BaseModel):
+    """ PriceEstimatesResponse model """
+    prices: List[PriceEstimate]

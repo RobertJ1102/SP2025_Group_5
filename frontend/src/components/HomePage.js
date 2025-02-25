95d25df514425ea8c3b093395d8a160805b9b7b9
RobertJ1102 <robert.jacobs@wustl.edu>
2025-02-25 13:39:07 -0600

    Update HomePage.js

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -5,53 +5,19 @@ import FareInputComponent from "./FareInputComponent";
 
 const HomePage = () => {
   return (
-    <>
-      {/* Full-Screen Background Map */}
-      <Box
-        sx={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          width: "100vw",
-          height: "100vh",
-          zIndex: 0,
-        }}
-      >
-        <MapComponent />
-      </Box>
-
-      {/* Content Container */}
-      <Container
-        maxWidth="xs"
-        sx={{
-          position: "absolute",
-          zIndex: 1,
-          color: "white",
-          bottom: 20,
-          right: 20,
-        }}
-      >
-        <Paper
-          elevation={3}
-          sx={{
-            padding: 3,
-            marginTop: 4,
-            textAlign: "center",
-            backgroundColor: "rgba(255, 255, 255, 0.9)",
-            backdropFilter: "blur(5px)",
-          }}
-        >
-          <Typography variant="h4" gutterBottom>
-            Welcome to FareFinder
-          </Typography>
-          <Typography variant="body1">Find the best nearby pickup location for your Uber ride.</Typography>
-          {/* Fare Input Component */}
-          <Box sx={{ marginTop: 2 }}>
-            <FareInputComponent />
-          </Box>
-        </Paper>
-      </Container>
-    </>
+    <Container maxWidth="sm" sx={{ mt: 4, mb: 4 }}>
+      <Paper elevation={3} sx={{ p: 2 }}>
+        <Typography variant="h4" align="center" gutterBottom>
+          FareFinder
+        </Typography>
+        {/* Map Section */}
+        <Box sx={{ width: "100%", height: "300px", mb: 2 }}>
+          <MapComponent />
+        </Box>
+        {/* Fare Input Section */}
+        <FareInputComponent />
+      </Paper>
+    </Container>
   );
 };
 

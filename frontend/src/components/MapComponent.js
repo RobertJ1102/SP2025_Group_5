de8e9cd7b44e290bdce1fe5646b5020f52172de0
RobertJ1102 <robert.jacobs@wustl.edu>
2025-03-04 16:07:06 -0600

    Increased UI Size
    This will probably be iterated more in the fuure

--- frontend/src/components/MapComponent.js
+++ frontend/src/components/MapComponent.js
@@ -145,7 +145,7 @@ const MapComponent = ({
     }
   }, [currentLocation, map]);
 
-  return <div ref={mapRef} style={{ width: "100%", height: "300px" }} />;
+  return <div ref={mapRef} style={{ width: "100%", height: "500px" }} />;
 };
 
 export default MapComponent;

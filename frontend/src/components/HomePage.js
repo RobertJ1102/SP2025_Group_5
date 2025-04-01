6881c94f5390706d8c2211f05599958430e22e79
RobertJ1102 <robert.jacobs@wustl.edu>
2025-04-01 16:48:28 -0500

    Update HomePage.js

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Container, Typography, Paper } from "@mui/material";
-import RouteEstimatorWithFields from "./RouteEstimatorWithFields";
 import WelcomeModal from "./WelcomeModal";
-import GoogleMap from "./GoogleMap";
+import RouteEstimatorWithWideMap from "./RouteEstimatorWithWideMap";
 
 const HomePage = () => {
   const [showWelcome, setShowWelcome] = useState(false);
@@ -22,19 +20,8 @@ const HomePage = () => {
   return (
     <>
       <WelcomeModal open={showWelcome} onClose={handleCloseWelcome} />
-      <Container maxWidth="sm" sx={{ mt: 4, mb: 4 }}>
-        <Paper elevation={3} sx={{ p: 2 }}>
-          <Typography variant="h4" align="center" gutterBottom>
-            FareFinder
-          </Typography>
-          {/* Map Section */}
-          {/* <Box sx={{ width: "100%", height: "300px", mb: 2 }}>
-            <MapComponent />
-          </Box> */}
-          {/* Fare Input Section */}
-          <RouteEstimatorWithFields />
-        </Paper>
-      </Container>
+      {/* Render the wide map component without a constraining container */}
+      <RouteEstimatorWithWideMap />
     </>
   );
 };

9b77449607f0676cc76f2c6d45c3e790dd15fec7
RobertJ1102 <robert.jacobs@wustl.edu>
2025-04-08 12:45:02 -0500

    remove old files

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Container, Typography } from "@mui/material";
 import WelcomeModal from "./WelcomeModal";
 import RouteEstimatorWithFields from "./RouteEstimatorWithFields";
-import RouteEstimatorOverlay from "./RouteEstimatorOverlay";
-import RouteEstimatorWithWideMap from "./RouteEstimatorWithWideMap";
-import RouteEstimatorTwoColumn from "./two";
 
 const HomePage = () => {
   const [showWelcome, setShowWelcome] = useState(false);
